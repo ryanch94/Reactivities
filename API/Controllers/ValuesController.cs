0bfa102e2b8c51053b4d5cc4414075457d11c8c5
Ryan Challis <ryanchallis1994@gmail.com>
2019-12-18 23:12:21 +0000

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+        private readonly DataContext _context;
+
+        public ValuesController(ILogger<ValuesController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+    }
+}
