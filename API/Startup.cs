882f9a14a7bd5ffd195c2b2d79dd2a65f8a6b95f
Ryan Challis <ryanchallis1994@gmail.com>
2019-12-20 22:58:47 +0000

    End of S3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
             {
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(options => options.AddPolicy("CorsPolicy", policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000")));
             services.AddControllers();
         }
 
@@ -49,6 +50,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
